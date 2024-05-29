1fc610e35e5d62b29a88c61657d73ad1a8fa58dd
eyu-dev <169823478+eyu-dev@users.noreply.github.com>
2024-05-29 14:48:01 +0300

    Initial commit
    Signed-off-by: eyu-dev <169823478+eyu-dev@users.noreply.github.com>

---
+++ src/main/java/com/et/pwsdemo/PwsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.et.pwsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PwsDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PwsDemoApplication.class, args);
+
+    }
+
+}
